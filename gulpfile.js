01773fd127b0434808c65f62e838b137de15412d
Eileen choi <eileen.h.choi@gmail.com>
2018-09-19 14:00:54 +0100

    updated Gulp for PHP WP

--- gulpfile.js
+++ gulpfile.js
@@ -1,196 +1,152 @@
-//initialize all of our variables
-var app, base, concat, directory, gulp, gutil, hostname, path, refresh, sass, uglify, imagemin, minifyCSS, del, autoprefixer, gulpSequence, shell, sourceMaps, plumber;
+// Gulp.js configuration
+'use strict';
 
-var autoPrefixBrowserList = ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'];
+const
 
-//load all of our dependencies
-//add more here if you want to include more libraries
-gulp = require('gulp');
-gutil = require('gulp-util');
-concat = require('gulp-concat');
-uglify = require('gulp-uglify');
-sass = require('gulp-sass');
-sourceMaps = require('gulp-sourcemaps');
-imagemin = require('gulp-imagemin');
-minifyCSS = require('gulp-minify-css');
-autoprefixer = require('gulp-autoprefixer');
-gulpSequence = require('gulp-sequence').use(gulp);
-shell = require('gulp-shell');
-plumber = require('gulp-plumber');
+    // source and build folders
+    dir = {
+        src: 'src/',
+        build: '/var/www/wp-content/themes/maker-pro/'
+    },
+
+    // Gulp and plugins
+    gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    newer = require('gulp-newer'),
+    imagemin = require('gulp-imagemin'),
+    sass = require('gulp-sass'),
+    postcss = require('gulp-postcss'),
+    deporder = require('gulp-deporder'),
+    concat = require('gulp-concat'),
+    stripdebug = require('gulp-strip-debug'),
+    uglify = require('gulp-uglify')
+    ;
+
+// Browser-sync
+var browsersync = false;
 
 
+// PHP settings
+const php = {
+    src: dir.src + 'template/**/*.php',
+    build: dir.build
+};
 
-//compressing images & handle SVG files
-gulp.task('images', function (tmp) {
-    gulp.src(['app/images/*.jpg', 'app/images/*.png'])
-        //prevent pipe breaking caused by errors from gulp plugins
-        .pipe(plumber())
-        .pipe(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true }))
-        .pipe(gulp.dest('app/images'));
+// copy PHP files
+gulp.task('php', () => {
+    return gulp.src(php.src)
+        .pipe(newer(php.build))
+        .pipe(gulp.dest(php.build));
 });
 
-//compressing images & handle SVG files
-gulp.task('images-deploy', function () {
-    gulp.src(['app/images/**/*', '!app/images/README'])
-        //prevent pipe breaking caused by errors from gulp plugins
-        .pipe(plumber())
-        .pipe(gulp.dest('dist/images'));
+// image settings
+const images = {
+    src: dir.src + 'images/**/*',
+    build: dir.build + 'images/'
+};
+
+// image processing
+gulp.task('images', () => {
+    return gulp.src(images.src)
+        .pipe(newer(images.build))
+        .pipe(imagemin())
+        .pipe(gulp.dest(images.build));
 });
 
-//compiling our Javascripts
-gulp.task('scripts', function () {
-    //this is where our dev JS scripts are
-    return gulp.src(['app/scripts/src/_includes/**/*.js', 'app/scripts/src/**/*.js'])
-        //prevent pipe breaking caused by errors from gulp plugins
-        .pipe(plumber())
-        //this is the filename of the compressed version of our JS
-        .pipe(concat('app.js'))
-        //catch errors
-        .on('error', gutil.log)
-        //where we will store our finalized, compressed script
-        .pipe(gulp.dest('app/scripts'))
-        
-});
-
-//compiling our Javascripts for deployment
-gulp.task('scripts-deploy', function () {
-    //this is where our dev JS scripts are
-    return gulp.src(['app/scripts/src/_includes/**/*.js', 'app/scripts/src/**/*.js'])
-        //prevent pipe breaking caused by errors from gulp plugins
-        .pipe(plumber())
-        //this is the filename of the compressed version of our JS
-        .pipe(concat('app.js'))
-        //compress :D
-        .pipe(uglify())
-        //where we will store our finalized, compressed script
-        .pipe(gulp.dest('dist/scripts'));
-});
-
-//compiling our SCSS files
-gulp.task('styles', function () {
-    //the initializer / master SCSS file, which will just be a file that imports everything
-    return gulp.src('app/styles/scss/init.scss')
-        //prevent pipe breaking caused by errors from gulp plugins
-        .pipe(plumber({
-            errorHandler: function (err) {
-                console.log(err);
-                this.emit('end');
-            }
-        }))
-        //get sourceMaps ready
-        .pipe(sourceMaps.init())
-        //include SCSS and list every "include" folder
-        .pipe(sass({
-            errLogToConsole: true,
-            includePaths: [
-                'app/styles/scss/'
-            ]
-        }))
-        .pipe(autoprefixer({
-            browsers: autoPrefixBrowserList,
-            cascade: true
-        }))
-        //catch errors
-        .on('error', gutil.log)
-        //the final filename of our combined css file
-        .pipe(concat('styles.css'))
-        //get our sources via sourceMaps
-        .pipe(sourceMaps.write())
-        //where to save our final, compressed css file
-        .pipe(gulp.dest('app/styles'))
-        
-});
-
-//compiling our SCSS files for deployment
-gulp.task('styles-deploy', function () {
-    //the initializer / master SCSS file, which will just be a file that imports everything
-    return gulp.src('app/styles/scss/init.scss')
-        .pipe(plumber())
-        //include SCSS includes folder
-        .pipe(sass({
-            includePaths: [
-                'app/styles/scss',
-            ]
-        }))
-        .pipe(autoprefixer({
-            browsers: autoPrefixBrowserList,
-            cascade: true
-        }))
-        //the final filename of our combined css file
-        .pipe(concat('styles.css'))
-        .pipe(minifyCSS())
-        //where to save our final, compressed css file
-        .pipe(gulp.dest('dist/styles'));
-});
-
-//basically just keeping an eye on all HTML files
-gulp.task('html', function () {
-    //watch any and all HTML files and refresh when something changes
-    return gulp.src('app/*.html')
-        .pipe(plumber())
-        //catch errors
-        .on('error', gutil.log);
-});
-
-//migrating over all HTML files for deployment
-gulp.task('html-deploy', function () {
-    //grab everything, which should include htaccess, robots, etc
-    gulp.src('app/*')
-        //prevent pipe breaking caused by errors from gulp plugins
-        .pipe(plumber())
-        .pipe(gulp.dest('dist'));
-
-    //grab any hidden files too
-    gulp.src('app/.*')
-        //prevent pipe breaking caused by errors from gulp plugins
-        .pipe(plumber())
-        .pipe(gulp.dest('dist'));
-
-    gulp.src('app/fonts/**/*')
-        //prevent pipe breaking caused by errors from gulp plugins
-        .pipe(plumber())
-        .pipe(gulp.dest('dist/fonts'));
-
-    //grab all of the styles
-    gulp.src(['app/styles/*.css', '!app/styles/styles.css'])
-        //prevent pipe breaking caused by errors from gulp plugins
-        .pipe(plumber())
-        .pipe(gulp.dest('dist/styles'));
-});
-
-//cleans our dist directory in case things got deleted
-gulp.task('clean', function () {
-    return shell.task([
-        'rm -rf dist'
-    ]);
-});
-
-//create folders using shell
-gulp.task('scaffold', function () {
-    return shell.task([
-        'mkdir dist',
-        'mkdir dist/fonts',
-        'mkdir dist/images',
-        'mkdir dist/scripts',
-        'mkdir dist/styles'
+// CSS settings
+var css = {
+    src: dir.src + 'scss/style.scss',
+    watch: dir.src + 'scss/**/*',
+    build: dir.build,
+    sassOpts: {
+        outputStyle: 'nested',
+        imagePath: images.build,
+        precision: 3,
+        errLogToConsole: true
+    },
+    processors: [
+        require('postcss-assets')({
+            loadPaths: ['images/'],
+            basePath: dir.build,
+            baseUrl: '/wp-content/themes/wptheme/'
+        }),
+        require('autoprefixer')({
+            browsers: ['last 2 versions', '> 2%']
+        }),
+        require('css-mqpacker'),
+        require('cssnano')
     ]
-    );
+};
+
+// CSS processing
+gulp.task('css', gulp.parallel('images'), () => {
+    return gulp.src(css.src)
+        .pipe(sass(css.sassOpts))
+        .pipe(postcss(css.processors))
+        .pipe(gulp.dest(css.build))
+        .pipe(browsersync ? browsersync.reload({ stream: true }) : gutil.noop());
 });
 
-//this is our master task when you run `gulp` in CLI / Terminal
-//this is the main watcher to use when in active development
-//  this will:
-//  startup the web server,
-//  compress all scripts and SCSS files
-gulp.task('default', gulp.series('scripts', 'styles', function (done) {
-    //a list of watchers, so it will watch all of the following files waiting for changes
-    gulp.watch('app/scripts/src/**', ['scripts']);
-    gulp.watch('app/styles/scss/**', ['styles']);
-    gulp.watch('app/images/**', ['images']);
-    gulp.watch('app/*.html', ['html']);
-    done();
-}));
+// JavaScript settings
+const js = {
+    src: dir.src + 'js/**/*',
+    build: dir.build + 'js/',
+    filename: 'scripts.js'
+};
+
+// JavaScript processing
+gulp.task('js', () => {
+
+    return gulp.src(js.src)
+        .pipe(deporder())
+        .pipe(concat(js.filename))
+        .pipe(stripdebug())
+        .pipe(uglify())
+        .pipe(gulp.dest(js.build))
+        .pipe(browsersync ? browsersync.reload({ stream: true }) : gutil.noop());
+
+});
+
+// run all tasks
+gulp.task('build', gulp.series('php', 'css', 'js'));
+
+// Browsersync options
+const syncOpts = {
+    proxy: 'localhost',
+    files: dir.build + '**/*',
+    open: false,
+    notify: false,
+    ghostMode: false,
+    ui: {
+        port: 8001
+    }
+};
 
 
-//this is our deployment task, it will set everything for deployment-ready files
-gulp.task('deploy', gulpSequence('clean', 'scaffold', ['scripts-deploy', 'styles-deploy', 'images-deploy'], 'html-deploy'));
+// browser-sync
+gulp.task('browsersync', () => {
+    if (browsersync === false) {
+        browsersync = require('browser-sync').create();
+        browsersync.init(syncOpts);
+    }
+});
+
+// watch for file changes
+gulp.task('watch', gulp.parallel('browsersync'), () => {
+
+    // page changes
+    gulp.watch(php.src, ['php'], browsersync ? browsersync.reload : {});
+
+    // image changes
+    gulp.watch(images.src, ['images']);
+
+    // CSS changes
+    gulp.watch(css.watch, ['css']);
+
+    // JavaScript main changes
+    gulp.watch(js.src, ['js']);
+
+});
+
+// default task
+gulp.task('default', gulp.series('build', 'watch'));
